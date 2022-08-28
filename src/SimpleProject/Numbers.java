11df39f9b1ba9474c947fbf14a3a4545f066a3f8
omid1 <omid.sst22@gmail.com>
2022-08-28 13:28:08 -0700

    this is my first commit

---
+++ src/SimpleProject/Numbers.java
@@ -0,0 +1,10 @@
+package SimpleProject;
+
+public class Numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i+=2) {
+            System.out.println(i);
+
+        }
+    }
+}
